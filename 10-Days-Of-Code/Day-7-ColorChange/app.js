e5b685752e94575375fb9a533533a27947cc1b07
Kristoff L <50643772+kristlittle@users.noreply.github.com>
2021-06-30 23:53:49 -0400

    updates pages

--- 10-Days-Of-Code/Day-7-ColorChange/app.js
+++ 10-Days-Of-Code/Day-7-ColorChange/app.js
@@ -1,17 +1,17 @@
 
 
 //COLORS ARRAY..
-let col = ['blue', 'red', 'orange', 'yellow', 'green'];
+let color = ['blue', 'red', 'orange', 'yellow', 'green'];
 
 //changing color of background
 let button = document.getElementById('button');
 
 button.addEventListener('click', function()
     {
-    let indx = parseInt((Math.random()*colors.length)+1);
+    let indx = parseInt((Math.random()*color.length)+1);
 
     let back = document.getElementById('back');
 
-    back.style.background = `${col[indx]}`
+    back.style.background = `${color[indx]}`
     }
 )
